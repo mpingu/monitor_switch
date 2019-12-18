03deee1b82d7d051f99a3920269df41ac42d3f3f
Carsten Bohuslav <mpingu@MpinguBook.fritz.box>
2019-12-18 19:30:45 +0100

    Added Switch to HDMI1

--- macOS/to_HDMI1.sh
+++ macOS/to_HDMI1.sh
@@ -2,7 +2,7 @@
 # cbohuslav 2019-12-19
 # switch Monitor Inputs!
 
-# Set Monitor 2 to Input DisplaypPort-2 ID=16
-ddcctl -d 2 -i 16
-# Set Monitor 1 to Input DisplaypPort-2 ID=16
-ddcctl -d 1 -i 16
+# Set Monitor 2 to Input HDMI-1 ID=17
+ddcctl -d 2 -i 17
+# Set Monitor 1 to Input HDMI-1 ID=17
+ddcctl -d 1 -i 17
